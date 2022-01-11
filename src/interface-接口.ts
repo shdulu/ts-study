3aeb0b6b1c3e2ab4c9c0737857c11f4ad8873f83
shdulu <17625329975>
2022-01-11 22:10:55 +0800

    update:接口

--- src/interface-接口.ts
+++ src/interface-接口.ts
@@ -1,11 +1,17 @@
 /**
  * 接口
+ * 接口一方面可以在面向对象编程中表示为行为的抽象，另外可以用来描述对象的形状
+ * 接口就是把一些类中共有的属性和方法抽象出来，可以用来约束实现此接口的类
+ * 一个类可以继承另一个类并实现多个接口
+ * 接口像插件一样是用来增强类的，而抽象类是具体类的抽象概念
+ * 一个类可以实现多个接口，一个接口可以被对个类实现，但一个类可以有多个子类，但只能有一个父类
  *
  * */
 export {};
-// 1. 描述对象的形状
+// 对象的形状
+// 1. 接口可以用来描述对象的形状，多属性或者少属性都会报错
 interface Speakable {
-  name: string;
+  name?: string; // ? 表示可选属性
   speak(): void;
 }
 
@@ -26,6 +32,7 @@ interface Eatable {
   eat(): void;
 }
 
+// 类实现(implements)接口
 class Person implements Speakable, Eatable {
   name!: string;
   speak() {
